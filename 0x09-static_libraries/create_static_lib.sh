473109339a4999c81a2a550406b1d2e4794b4c85
AKRAM-2002 <75812240+AKRAM-2002@users.noreply.github.com>
2023-04-10 05:09:15 +0000

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Compile all .c files into object files
-gcc -c *.c
+gcc -Wall -pedantic -Werror -Wextra -c *.c
 
 # Create the static library liball.a from the object files
-ar rc liball.a *.o
+ar -rc liball.a *.o
+
+ranlib liball.a
 
