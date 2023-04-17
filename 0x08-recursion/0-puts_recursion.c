cb187a56baf32785630ec9702e5c27cfb44c09aa
akram <akram@gmail.com>
2023-04-16 17:01:09 -0700

    push 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,13 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == EOF)
-	    _putchar('\n');
-
-	else{
-	       	_putchar(*s);
+	if (*s)
+	{
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 
-
+	else
+		_putchar('\n');
 }
 
