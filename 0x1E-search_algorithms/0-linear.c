18ba3f4b26f1200e52486e254108f8dd1d797356
akram <akramboutzouga@gmail.com>
2024-05-08 14:56:45 +0100

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,6 +18,12 @@
 int linear_search(int *array, size_t size, int value){
 
     int i;
+
+    if (array == NULL)
+    {
+        return -1;
+    }
+    
     for(i = 0; i < (int)size; i++){
         printf("Value Checked array[%u] = %d\n", i, array[i]);
         if(array[i] == value){
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/*
+ * Function: binary_search
+ * ------------------------
+ * Description: Implements the Binary search algorithm to find a value in a sorted array of integers.
+ * 
+ * Parameters:
+ *   - array: input array
+ *   - size: size of the array
+ *   - value: value to search for
+ * 
+ * Returns:
+ *   - Always EXIT_SUCCESS
+ */
+
+
+int binary_search(int *array, size_t size, int value){
+
+    if (array == NULL)
+    {
+        return -1;
+    }
+
+}
