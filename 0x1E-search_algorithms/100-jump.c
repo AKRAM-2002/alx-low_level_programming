98a7c9643fbc3e1fe867cc0e2041373e8358b890
akram <akramboutzouga@gmail.com>
2024-05-11 21:39:25 +0100

    update

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,20 @@
+#include "search_algos.h"
+#include <math.h>
+/*
+ * Function: binary_search
+ * ------------------------
+ * Description: Implements the Jump search algorithm to find a value in a sorted array of integers.
+ * 
+ * Parameters:
+ *   - array: input array
+ *   - size: size of the array
+ *   - value: value to search for
+ * 
+ * Returns:
+ *   - Always EXIT_SUCCESS
+ */
+
+int jump_search(int *array, size_t size, int value){
+    
+
+}
